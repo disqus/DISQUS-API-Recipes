9d16f4edc7544357d7988cf24a639069c0e68f43
Talton Figgins <talton@disqus.com>
2012-08-07 13:00:34 -0700

    Uploading OAuth Example

---
+++ php/oauth/oauth-test.php
@@ -0,0 +1,8 @@
+<?php
+
+// This should redirect to the second OAuth file provided
+echo "<a href='https://disqus.com/api/oauth/2.0/authorize/?client_id=YOUR_PUBLIC_KEY&scope=read,write&response_type=code&redirect_uri=http://www.example.com/oauth-callback.php'>OAuth</a>";
+
+   ?>
+
+
